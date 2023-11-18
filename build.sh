770e9a3304cf287b34b9e4dab0651d3e62fabf7a
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-19 00:16:09 +0530

    added

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+export PATH=$PATH:/usr/local/python3/bin
 pip install gunicorn==20.1.0
 python manage.py collectstatic --no-input
 python manage.py migrate
