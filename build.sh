171c2f14b920e67a698465e6b45ba136f963f571
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-19 00:42:38 +0530

    added

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+pip install git+https://github.com/benoitc/gunicorn.git
 export PATH=$PATH:/usr/local/python3/bin
 pip install gunicorn==20.1.0
 python manage.py collectstatic --no-input
