cb4faaad319755018eff4e07d698b1581f4d45d5
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-18 22:46:14 +0530

    added

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -149,5 +149,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # }
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'keeping/build/static')
+    os.path.join(BASE_DIR,'keeping/build/static/')
 ]
