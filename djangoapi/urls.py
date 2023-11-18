7f5324a40aea0bf99ce32af6d8ed1c95dd8e58ac
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-18 23:47:20 +0530

    added static

--- djangoapi/urls.py
+++ djangoapi/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+import settings
+from django.conf.urls.static import static
+
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
     path('api/',include('todoap.urls')),
     path("api-auth/", include("rest_framework.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
