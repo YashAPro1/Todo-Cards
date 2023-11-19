a58ce23a96c207ec8b9dbcb6c6fb80d70e618815
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-19 23:14:47 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-pip install -r requirements.txt
-pip install git+https://github.com/benoitc/gunicorn.git
-pip install gunicorn==20.1.0
 echo $PATH
 export PATH=$PATH:/opt/render/.local/bin
+pip install -r requirements.txt
+
 echo "Hi"
+echo $PATH
+pip install gunicorn==20.1.0
 echo $PATH
 python manage.py collectstatic --no-input
 python manage.py migrate
