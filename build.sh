cb5acb9066fa46bea3ceaf1079e781f17b4b080a
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-20 00:15:32 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 echo $PATH
 export PATH=$PATH:/opt/render/.local/bin
-export PATH=$PATH:/usr/local/python3/bin
 pip install -r requirements.txt
 
 echo "Hi"
@@ -13,5 +12,3 @@ echo $PATH
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-cd /usr/local/python3/bin
-gunicorn app:app
