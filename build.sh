36eeab31bb9e3ba05e8e07192c689584983075b9
YashAPro1 <rekhasd01000001@gmail.com>
2023-11-20 15:25:49 +0530

    builded again1

--- build.sh
+++ build.sh
@@ -11,3 +11,5 @@ echo $PATH
 python manage.py collectstatic --no-input
 python manage.py migrate
 pip install gunicorn==20.1.0
+gunicorn --bind 127.0.0.1:8080 djangoapi.wsgi:application
+python manage.py runserver
